020bf597ed088ed753f95c986dd1183e02becd8b
Rogerio Pelizari <rogpelizari@hotmail.com>
2021-02-17 13:13:29 -0300

    Header ajustes routemodule, assets-images-logo #4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashBoardComponent } from './pages/dash-board/dash-board.component';
 
-const routes: Routes = [{ path: 'dashboard', component: DashBoardComponent }];
+const routes: Routes = [
+  { path : '', redirectTo: '/dashboard', pathMatch:"full" },
+  { path: 'dashboard', component: DashBoardComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
