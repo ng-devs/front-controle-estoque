504a60c50bd9ca9cde9c8777a9fc1a8528068c27
Gleison de Almeida <48810597+gleisonkz@users.noreply.github.com>
2021-03-04 23:26:37 -0300

    ✨ feat: add button click event

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,17 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent implements OnInit {
   constructor() {}
   @Input() btnBgColor: string;
+  @Input() btnText: string;
   @Input() contentColor: string;
   @Input() title: string;
   @Input() value: number;
   @Input() extraData: string;
+  @Output() btnEvent = new EventEmitter<void>();
 
   ngOnInit(): void {}
 }
