902380bfc8d71d06e527181ff865e709a1d7cded
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-03-10 23:45:09 -0300

    ✨ feat: criação do componente forma de pagamento

---
+++ src/app/pages/cadastros/forma-pagamento/forma-pagamento.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  template: `<router-outlet></router-outlet>`
+})
+export class FormaPagamentoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+}
