a5c54c1db1c47b7e49aaf9466896bba0cb268f9e
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-03-10 23:40:53 -0300

    ✨ feat(forma de pagamento): criação do componente de create edit

---
+++ src/app/pages/cadastros/forma-pagamento/create-edit/create-edit.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './create-edit.component.html',
+  styleUrls: ['./create-edit.component.scss']
+})
+export class CreateEditComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
