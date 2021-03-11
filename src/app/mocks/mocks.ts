611f75df732b43cade41bd3a9639748c5ac8c429
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-03-10 23:38:55 -0300

    ✨ feat: criação de novo mock de forma de pagamento

--- src/app/mocks/mocks.ts
+++ src/app/mocks/mocks.ts
@@ -1,6 +1,7 @@
 import { ProductStockListItem, SaleListItem } from 'src/app/models';
 import { Category, PaymentMethod } from 'src/app/models/enums';
 import { ProductSoldListItem } from 'src/app/models/product-sold-list-item';
+import { PaymentShape } from '../pages/cadastros/forma-pagamento/models/payment-shape.model';
 
 export const PRODUCTS_SOLD_MOCK: ProductSoldListItem[] = [
   { id: 1, name: 'Estojo', category: Category.Papelaria, quantity: 300, price: 3.5 },
@@ -18,4 +19,10 @@ export const SALES_MOCK: SaleListItem[] = [
   { id: 1, date: '01/01', value: 350, paymentMethod: PaymentMethod.Cash },
   { id: 2, date: '05/01', value: 230, paymentMethod: PaymentMethod.DebitCard },
   { id: 3, date: '10/01', value: 50, paymentMethod: PaymentMethod.CreditCard },
+];
+
+export const PAYMENT_SHAPE: PaymentShape[] = [
+  { id: 1, description: 'Cartão de crédito' },
+  { id: 2, description: 'PIX' },
+  { id: 3, description: 'Boleto' },
 ];
