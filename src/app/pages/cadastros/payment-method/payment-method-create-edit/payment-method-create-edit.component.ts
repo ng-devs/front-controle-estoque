d68369353660c719a790e71d066fa875a5ac38f8
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-03-24 22:37:43 -0300

    📦 refactor: padronização do nome dos componentes

---
+++ src/app/pages/cadastros/payment-method/payment-method-create-edit/payment-method-create-edit.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './payment-method-create-edit.component.html',
+  styleUrls: ['./payment-method-create-edit.component.scss'],
+})
+export class PaymentMethodCreateEditComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
