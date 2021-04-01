77abd98a56c2e2bdccd9e16702ab7fea4bc40422
Hernandes Junio de Assis <hernandesjunio@gmail.com>
2021-04-01 17:39:48 -0300

    ✨ feat: adição de filtro
    resolves #40

--- src/app/pages/cadastros/sales/sales-list/sales-list.component.ts
+++ src/app/pages/cadastros/sales/sales-list/sales-list.component.ts
@@ -1,20 +1,33 @@
-import { Component, ViewChild } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, forwardRef, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { GenericListComponent, ModalComponent } from '@app/components';
 import { SALES_MOCK } from '@app/mocks';
 import { Action, ActionEvent, SaleListItem, TableColumns } from '@app/models';
 import { SaleProductListItemDetail } from 'app/models/sale-product-list-item-detail';
+import { merge } from 'rxjs';
+import { debounceTime, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'sales-list',
   templateUrl: './sales-list.component.html',
   styleUrls: ['./sales-list.component.scss'],
+  animations: [
+    trigger('displayFilter', [
+      transition(':enter', [
+        style({ opacity: 0, transform: 'translateX(-100%)' }),
+        animate('100ms', style({ opacity: 1, transform: 'translateX(0)' })),
+      ]),
+      transition(':leave', [animate('100ms', style({ opacity: 0, transform: 'translateX(-100%)' }))]),
+    ]),
+  ],
 })
-export class SalesListComponent {
+export class SalesListComponent implements OnInit {
   @ViewChild('salesRef') private $salesModalComponent: ModalComponent;
   @ViewChild('customMatTable') private $table: GenericListComponent<SaleProductListItemDetail>;
   dataSource = SALES_MOCK;
-
+  currentSaleListItem: SaleListItem;
   productColumns: TableColumns<SaleListItem> = {
     id: { label: '#' },
     date: { label: 'Data', pipe: { name: 'date', args: 'dd/MM/yyyy' } },
@@ -38,18 +51,69 @@ export class SalesListComponent {
     total: { label: 'Valor total', pipe: { name: 'currency' } },
   };
 
-  constructor(private router: Router) {}
+  constructor(private router: Router) {
+
+  }
+
+  ngOnInit(): void {
+
+    merge(this.filterGroup.controls.valorStart.valueChanges, this.filterGroup.controls.valorEnd.valueChanges)
+      .pipe(debounceTime(500))
+      .subscribe(() => this.filtrarValor());
+
+    merge(this.filterGroup.controls.dataStart.valueChanges, this.filterGroup.controls.dataEnd.valueChanges)
+      .pipe(debounceTime(500))
+      .subscribe(() => this.filtrarData());
+  }
 
   add() {
     this.router.navigateByUrl('vendas/create-edit');
   }
 
-  currentSaleListItem: SaleListItem;
-
   showDetails(action: ActionEvent<SaleListItem>) {
-    // this.$salesModalComponent.title = `Detalhes da venda ${action.item.id}`;
     this.currentSaleListItem = action.item;
     this.$table.tableData = action.item.products!;
     this.$salesModalComponent.open();
   }
+
+  displayValor = false;
+  displayCalendar = false;
+
+  filterGroup = new FormGroup({
+    dataStart: new FormControl('', Validators.required),
+    dataEnd: new FormControl('', Validators.required),
+    valorStart: new FormControl('', Validators.required),
+    valorEnd: new FormControl('', Validators.required),
+  });
+
+  filtrarData() {
+    let source = SALES_MOCK;
+    const { dataStart, dataEnd } = this.filterGroup.controls;
+    if (dataStart.valid && dataEnd.valid) {
+      source = SALES_MOCK.filter((c) => c.date >= dataStart.value && c.date <= dataEnd.value);
+    }
+    this.dataSource = source;
+  }
+
+  filtrarValor() {
+    let source = SALES_MOCK;
+    const { valorStart, valorEnd } = this.filterGroup.controls;
+    if (valorStart.valid && valorEnd.valid) {
+      source = SALES_MOCK.filter((c) => c.value >= +valorStart.value && c.value <= +valorEnd.value);
+    }
+    this.dataSource = source;
+  }
+
+  displayFilterRangeValue() {
+    this.filterGroup.reset();
+    this.displayCalendar = false;
+    this.displayValor = !this.displayValor;
+  }
+
+  displayFilterRangeDate() {
+    this.filterGroup.reset();
+    this.displayValor = false;
+    this.displayCalendar = !this.displayCalendar;
+  }
 }
+
